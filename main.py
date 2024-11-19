9c506892489c678c22323c0a3e06e926a7e412ef
Vedant <tarunincarnate@gmail.com>
2024-11-19 19:12:26 +0530

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def send(question, url, webhook_url):
     response = requests.post(webhook_url, json=json)
 
 if __name__ == "__main__":
-    webhook_url = "https://discord.com/api/webhooks/1308327826545704991/XUXXvdGkJxN-7EhLnE2wS9daDfcZcrwll0sqNDDkcapbHx5Bm6P0Lpmb5Aw9ceR_OYSa"
+    webhook_url = "https://discord.com/api/webhooks/1308426087331663953/Ylj_p9qaEVBSq_Cfifkl31KsWhjyCU4A2d3NTcRp_hlX4WR6K9-gi1Nux4iSfIMJFOde"
 
     send(
         "The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying Eureka? Because this sum gives the same number",
